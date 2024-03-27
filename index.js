cb1ff7b96baf34fa459cddad33ef667c0ee6cf1b
Carlos J Guzman <152306129+cjavier7@users.noreply.github.com>
2024-03-27 02:51:24 +0000

    corregido

--- index.js
+++ index.js
@@ -1,22 +1,22 @@
 let iconos = ["♦", "♥", "♠", "♣"];
 let numeros = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13];
-
-
-
 let cartas = [];
 
 let draw = document.querySelector("#draw");
 let sort = document.querySelector("#sort");
 
-function generadorCartas() {
-
-    cartas = [];
-    let parrilla = document.querySelector("#parrilla");
-
-    parrilla.innerHTML = "";
+draw.addEventListener('click', generarCartas)
+sort.addEventListener('click', ordenarCartas) 
 
 
-    let numeroCartas = parseInt(document.getElementById("nCartas").value);
+function generarCartas() {
+
+    let container = document.querySelector("#container");
+
+    container.innerHTML = "";
+
+
+    let numeroCartas = parseInt(document.getElementById("amountcards").value);
     for (i = 1; i <= numeroCartas; i++) {
         function conseguirIcono() {
             let numero = Math.floor(Math.random() * iconos.length);
@@ -37,37 +37,30 @@ function generadorCartas() {
         console.log(numero);
 
 
-        /*let iconoIzq = document.querySelector(".iconLeft");
-       let contenidoNumero = document.querySelector(".number");
-       let iconoDerech = document.querySelector(".iconRight"); */
-
-
-
-
 
         let cartaNueva = document.createElement("div");
-        let carta = parrilla.appendChild(cartaNueva).classList.add("card");
-        let iconoIzquierda = document.createElement("div")
-        let iconoIzq = cartaNueva.appendChild(iconoIzquierda).classList.add("iconLeft");
-        iconoIzquierda.innerHTML = icono;
+        let carta = container.appendChild(cartaNueva).classList.add("card");
+        let iconoArriba = document.createElement("div")
+        let iconoIzq = cartaNueva.appendChild(iconoArriba).classList.add("iconoArriba");
+        iconoArriba.innerHTML = icono;
         let contenidoNumero = document.createElement("div")
         let elNumero = cartaNueva.appendChild(contenidoNumero).classList.add("number");
-        contenidoNumero.innerHTML = changeValiu(numero);
-        let iconoDerech = document.createElement("div")
-        let iconoDere = cartaNueva.appendChild(iconoDerech).classList.add("iconRight");
-        iconoDerech.innerHTML = icono;
+        contenidoNumero.innerHTML = changeValue(numero);
+        let iconoAbajo = document.createElement("div")
+        let iconoDere = cartaNueva.appendChild(iconoAbajo).classList.add("iconoAbajo");
+        iconoAbajo.innerHTML = icono;
 
         if (icono == "♦" || icono == "♥") {
-            iconoIzquierda.style.color = "red";
-            iconoDerech.style.color = "red";
+            iconoArriba.style.color = "red";
+            iconoAbajo.style.color = "red";
         }
 
-        let cartaValor = {
+        let cartaConseguida = {
             iCono: icono,
             nUmero: numero,
         };
-        cartas.push(cartaValor);
-        console.log(cartaValor);
+        cartas.push(cartaConseguida);
+        console.log(cartaConseguida);
     }
 
     console.log(cartas);
@@ -76,65 +69,72 @@ function generadorCartas() {
 }
 
 
-draw.addEventListener("click", generadorCartas);
 
-cartas = generadorCartas();
+cartas = generarCartas(); 
 
-
-
-
-
-
-
-
-function organizadorCartas() {
+function ordenarCartas() {
+    
     let ordenado = document.querySelector("#ordenado");
     const len = cartas.length;
     ordenado.innerHTML = "";
+    
+    
+    for (let i = 0; i < len - 1; i++) {
 
-    for (let i = 0; i < len - 1; i++) { // las veces que se va a repetir
-        for (let j = 0; j < len - 1; j++) { // comparando los elementos
-            if (cartas[j].nUmero > cartas[j + 1].nUmero) {
-                // variable auxiliares
-                const temp = cartas[j]; // 21
+       
+        for (let j = 0; j < len - 1; j++) { 
+
+            if (cartas[j].nUmero> cartas[j + 1].nUmero) {
+
+
+                const temp = cartas[j]; 
                 cartas[j] = cartas[j + 1];
                 cartas[j + 1] = temp;
             }
 
             contenedor = document.createElement("div");
-            contenedor.style.display = "flex";
-
-            for (let x = 0; x < len; x++) {
-
+            contenedor.style.display = "flex" 
+            
+            for (x=0; x < len; x++){
+                
                 ordenado.appendChild(contenedor);
                 let cartaNueva = document.createElement("div");
                 let carta = contenedor.appendChild(cartaNueva);
                 cartaNueva.classList.add("card");
-                let iconoIzquierda = document.createElement("div")
-                let iconoIzq = cartaNueva.appendChild(iconoIzquierda).classList.add("iconLeft");
-                iconoIzquierda.innerHTML = cartas[x].iCono;
+                let iconoArriba = document.createElement("div")
+                let iconoIzq = cartaNueva.appendChild(iconoArriba).classList.add("iconoArriba");
+                iconoArriba.innerHTML = cartas[x].iCono;
                 let contenidoNumero = document.createElement("div")
                 let elNumero = cartaNueva.appendChild(contenidoNumero).classList.add("number");
-                contenidoNumero.innerHTML = changeValiu(cartas[x].nUmero);
-                let iconoDerech = document.createElement("div")
-                let iconoDere = cartaNueva.appendChild(iconoDerech).classList.add("iconRight");
-                iconoDerech.innerHTML = cartas[x].iCono;
+                contenidoNumero.innerHTML = changeValue(cartas[x].nUmero);
+                let iconoAbajo = document.createElement("div")
+                let iconoDere = cartaNueva.appendChild(iconoAbajo).classList.add("iconoAbajo");
+                iconoAbajo.innerHTML = cartas[x].iCono;
                 if (cartas[x].iCono == "♦" || cartas[x].iCono == "♥") {
-                    iconoIzquierda.style.color = "red";
-                    iconoDerech.style.color = "red";
+                    iconoArriba.style.color = "red";
+                    iconoAbajo.style.color = "red";
                 }
-
+                
+            
+                console.log(contenedor);
+    
             }
+
+
         }
+        
+       
+
     }
+
     console.log(cartas);
     return cartas
-}
+    
+
+};
 
 
-
-
-function changeValiu(value) {
+function changeValue(value) {
     switch (value) {
         case 1: return "A";
         case 11: return "J";
@@ -143,9 +143,4 @@ function changeValiu(value) {
 
         default: return value;
     }
-}
-
-
-
-
-sort.addEventListener("click", organizadorCartas);
+} 
